4c91b57536bdfe40012699b544bc8f11c1a179e9
oladayocollins565-create <oladayocollins565@gmail.com>
2026-06-14 22:15:34 +0100

    Refactor weather notification logic and add temperature
    Updated weather forecast logic and messaging format.

--- main.py
+++ main.py
@@ -8,37 +8,61 @@ account_sid = os.environ.get("ACCOUNT_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 
 parameter = {
-    "lat": 5.603717,
-    "lon": -0.186964,
+    "lat": 7.484120,
+    "lon": 4.556276,
     "appid": api_key,
+    "units": "metric",
     "cnt": 4,
 }
 
-r = requests.get(OWM_Endpoint , params=parameter)
-r.raise_for_status()
-weather_data = r.json()
+response = requests.get(OWM_Endpoint , params=parameter)
+response.raise_for_status()
+weather_data = response.json()
 
 will_rain = False
+
+first_forecast = weather_data["list"][0]
+
+temperature = first_forecast["main"]["temp"]
+weather_condition = first_forecast["weather"][0]["description"].title()
+
+
 for hour_data in weather_data["list"]:
     condition_code = hour_data["weather"][0]["id"]
-    print(condition_code)
-    if int(condition_code) < 700:
+
+    if condition_code < 700:
         will_rain = True
         break
 
-msg= "It will rain today, take an umbrella with you."
 if will_rain:
-    client = Client(account_sid, auth_token)
-    sms_message = client.messages.create(
-        body="It's going to rain today. Remember to bring an umbrella ☂️",
-        from_= "+14058745904",
-        to= "+2347072850274"
+    message_body =(
+        f"🌦 Weather Update\n\n"
+        f"Condition: {weather_condition}\n"
+        f"Temperature: {temperature}°C\n"
+        f"Rain Expected: Yes ☂️\n\n"
+        f"It's going to rain today. Remember to bring an umbrella ☂️"
     )
-    print(sms_message.status)
+else:
+    message_body =(
+        f"☀️ Weather Update\n\n"
+        f"Condition: {weather_condition}\n"
+        f"Temperature: {temperature}°C\n"
+        f"Rain Expected: No\n\n"
+        f"It seem there will not be rain today, Enjoy your day!"
+    )
 
+client = Client(account_sid, auth_token)
+
+numbers = [
+    "whatsapp:+2347072850274",
+    "whatsapp:+2347078699595",
+    "whatsapp:+2349136779676"
+]
+
+for number in numbers:
     whatsapp_message = client.messages.create(
         from_="whatsapp:+14155238886",
-        body="It's going to rain today. Remember to bring an umbrella ☂️",
-        to="whatsapp:+2347072850274"
+        body=message_body,
+        to=number
     )
     print(whatsapp_message.status)
